811f62b1b3ab50993dedddafb169c11549d3b787
Iryna_Luchyn <luchyn.ira@gmail.com>
2025-03-15 15:54:02 +0100

    Table in students

---
+++ html/scripts.js
@@ -0,0 +1,9 @@
+// Show the modal when the Add button is clicked
+function showModal() {
+    document.getElementById("modal").style.display = "flex";
+}
+
+// Close the modal when the Close button is clicked
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
